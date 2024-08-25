9c7db18f72ebd32a11af4b6e1dca6a2455065d2c
HenryNguyen5 <6404866+HenryNguyen5@users.noreply.github.com>
2024-08-25 15:13:18 -0700

    Reenable test vector for pseudo-version

--- apps/go-mod-validator/test/pseudo-version.test.ts
+++ apps/go-mod-validator/test/pseudo-version.test.ts
@@ -31,28 +31,52 @@ import { it, describe, expect } from "vitest";
 
 // https://cs.opensource.google/go/x/mod/+/refs/tags/v0.20.0:module/pseudo_test.go
 const pseudoTests = [
-  // {major:"",      older: "",                      version: "v0.0.0-20060102150405-hash"},
-  // {major:"v0",    older: "",                      version: "v0.0.0-20060102150405-hash"},
-  // {major:"v1",    older: "",                      version: "v1.0.0-20060102150405-hash"},
-  // {major:"v2",    older: "",                      version: "v2.0.0-20060102150405-hash"},
-  // {major:"unused",older: "v0.0.0",                version: "v0.0.1-0.20060102150405-hash"},
-  // {major:"unused",older: "v1.2.3",                version: "v1.2.4-0.20060102150405-hash"},
+  { major: "", older: "", version: "v0.0.0-20060102150405-hash" },
+  { major: "v0", older: "", version: "v0.0.0-20060102150405-hash" },
+  { major: "v1", older: "", version: "v1.0.0-20060102150405-hash" },
+  { major: "v2", older: "", version: "v2.0.0-20060102150405-hash" },
+  { major: "unused", older: "v0.0.0", version: "v0.0.1-0.20060102150405-hash" },
+  { major: "unused", older: "v1.2.3", version: "v1.2.4-0.20060102150405-hash" },
   {
     major: "unused",
     older: "v1.2.99999999999999999",
     version: "v1.2.100000000000000000-0.20060102150405-hash",
   },
-  // {major:"unused",older: "v1.2.3-pre",     version: "v1.2.3-pre.0.20060102150405-hash"},
-  // {major:"unused",older: "v1.3.0-pre",     version: "v1.3.0-pre.0.20060102150405-hash"},
-  // {major:"unused",older: "v0.0.0--",       version: "v0.0.0--.0.20060102150405-hash"},
-  // {major:"unused",older: "v1.0.0+metadata",version: "v1.0.1-0.20060102150405-hash+metadata"},
-  // {major:"unused",older: "v2.0.0+incompatible", version: "v2.0.1-0.20060102150405-hash+incompatible"},
-  // {major:"unused",older: "v2.3.0-pre+incompatible",version: "v2.3.0-pre.0.20060102150405-hash+incompatible"},
+  {
+    major: "unused",
+    older: "v1.2.3-pre",
+    version: "v1.2.3-pre.0.20060102150405-hash",
+  },
+  {
+    major: "unused",
+    older: "v1.3.0-pre",
+    version: "v1.3.0-pre.0.20060102150405-hash",
+  },
+  {
+    major: "unused",
+    older: "v0.0.0--",
+    version: "v0.0.0--.0.20060102150405-hash",
+  },
+  {
+    major: "unused",
+    older: "v1.0.0+metadata",
+    version: "v1.0.1-0.20060102150405-hash+metadata",
+  },
+  {
+    major: "unused",
+    older: "v2.0.0+incompatible",
+    version: "v2.0.1-0.20060102150405-hash+incompatible",
+  },
+  {
+    major: "unused",
+    older: "v2.3.0-pre+incompatible",
+    version: "v2.3.0-pre.0.20060102150405-hash+incompatible",
+  },
 ];
 
 describe("isPseudoVersion", () => {
   for (const { version, older } of pseudoTests) {
-    it("should return true for pseudo versions", () => {
+    it(`should return true for pseudo version ${version} and false for non-pseudo version ${older}`, () => {
       const isPseudoV = isPseudoVersion(version);
       const isPseudoOlder = isPseudoVersion(older);
       expect(isPseudoV, `expected ${version} to be a pseudo version`).toBe(
@@ -66,7 +90,7 @@ describe("isPseudoVersion", () => {
   }
 
   for (const { version, older } of pseudoTests) {
-    it("should return the correct rev for pseudo versions", () => {
+    it(`should return the correct rev of "hash" for pseudo version ${version}`, () => {
       const rev = pseudoVersionRev(version);
       expect(rev, `expected ${version} to have rev "hash"`).toBe("hash");
       expect(() => pseudoVersionRev(older)).toThrowError();
