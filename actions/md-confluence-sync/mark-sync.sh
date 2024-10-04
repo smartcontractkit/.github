0ccb62244ff4775dcb58992fd456e404bf38567a
chainchad <96362174+chainchad@users.noreply.github.com>
2024-10-04 12:12:36 -0400

    Add validation for env vars and make input names consistent

--- actions/md-confluence-sync/mark-sync.sh
+++ actions/md-confluence-sync/mark-sync.sh
@@ -1,10 +1,42 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# $USER is your smartcontract email
-# $PASSWORD is the api token created on the https://id.atlassian.com/manage-profile/security/api-tokens page
-# Mermaid diagrams are rendered locally and inlined into the HTML artifact as PNGs
+##
+# Validation
+##
 
+if [[ -z "${FILES:-}" ]]; then
+  echo "FILES is not set. Exiting."
+  exit 1
+fi
+
+# $USER is the email address associated with the token.
+if [[ -z "${USER:-}" ]]; then
+  echo "USER is not set. Exiting."
+  exit 1
+fi
+
+if [[ -z "${TOKEN:-}" ]]; then
+  echo "TOKEN is not set. Exiting."
+  exit 1
+fi
+
+if [[ -z "${BASE_URL:-}" ]]; then
+  echo "BASE_URL is not set. Exiting."
+  exit 1
+fi
+
+if [[ -z "${SPACE:-}" ]]; then
+  echo "SPACE is not set. Exiting."
+  exit 1
+fi
+
+if [[ -z "${PARENT:-}" ]]; then
+  echo "PARENT is not set. Exiting."
+  exit 1
+fi
+
+# Mermaid diagrams are rendered locally and inlined into the HTML artifact as PNGs.
 mark -f "$FILES" \
   --edit-lock \
   --title-from-h1 \
