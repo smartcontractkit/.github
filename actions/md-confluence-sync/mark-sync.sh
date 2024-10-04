e9471628f0a218a6460c15b1b13b8d9db2c633e2
Radek Scheibinger <radek.scheibinger@smartcontract.com>
2024-10-04 11:38:15 +0200

    Fixes

--- actions/md-confluence-sync/mark-sync.sh
+++ actions/md-confluence-sync/mark-sync.sh
@@ -5,12 +5,12 @@ set -euo pipefail
 # $PASSWORD is the api token created on the https://id.atlassian.com/manage-profile/security/api-tokens page
 # Mermaid diagrams are rendered locally and inlined into the HTML artifact as PNGs
 
-mark -f "$FIILES" \
+mark -f "$FILES" \
   --edit-lock \
   --title-from-h1 \
   -u "$USER" \
   -p "$TOKEN" \
-  -b https://smartcontract-it.atlassian.net/wiki/ \
+  -b "$BASE_URL" \
   --space "$SPACE" \
   --parents "$PARENT" \
   --mermaid-provider mermaid-go
