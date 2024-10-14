d125ca9fe5e3b410de7c6db4a4ce3ed7a0728cd6
KalanyuZ <kalanyuz@users.noreply.github.com>
2024-10-14 23:23:32 +0900

    action error reporter 0.3.0: Support multi-section + long log trimming + UX improvements (#665)
    * support multi-section + long logs trimming + 4o
    
    * fix dry issue + add link to aer run
    
    Co-authored-by: chainchad <96362174+chainchad@users.noreply.github.com>
    
    * remove metric collection action
    
    * add shebang and failure mode to aer script
    
    * make auxiliary script executable
    
    * formatting + additional explanation
    
    * use dynamic path for scripts
    
    ---------
    
    Co-authored-by: chainchad <96362174+chainchad@users.noreply.github.com>

--- actions/llm-action-error-reporter/aer.sh
+++ actions/llm-action-error-reporter/aer.sh
@@ -1,27 +1,35 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 # Read all input from standard input
 input=$(cat)
 
 log_output=$(echo $input | jq -Rs . | sed 's/^"//;s/"$//' | sed -e 's/\\ No newline at end of file//g' | sed -E 's/\\+$//g' )
-analyze_prompt="
-You are a helpful expert data engineer with expertise in Blockchain and Decentralized Oracle Networks and software development. Your task is to analyze log records of a Github Action run and find plausible cause for error(s) that caused the run to fail. You will focus only on error logs.
 
-Given logs below tell me the following:
-1. Organize the errors by issue in order of appearance. Focus only on errors. Ignore warnings. Title each error as a 1 <= 10 words sentence that describes the error.
-  1.1 Show the source of errors in a single code block. You can remove parts of the logs that are very repetitive.
+analyze_prompt="
+You are a helpful expert data engineer with expertise in Blockchain and Decentralized Oracle Networks and software development. 
+Your task is to analyze log records of a Github Action run and find plausible cause for error(s) that caused the run to fail. 
+You will focus only on error logs which format goes by: [job name] [step name] [timestamp] [error message].
+
+Given logs below do the following:
+Organize the errors by issue in order of appearance. Focus only on errors. Ignore warnings.
+  ### 1. [A 1 <= 10 words sentence that describes the error]:[job id where the error happened]
+  **Source of Error** in a code block with the error and its relevant context (<= 10 lines). You can remove parts of the logs that are very repetitive.
     1.1.1 Wrap the code block like this:
     ```
     # error logs here
     ```
-  1.2 Write a **why**: a <= 100 words paragraph explaining what caused the error.
-  1.3 Write a **Suggested fix**: <= 100 words paragraph suggesting the fix for specific the error, if any. Do not discuss anything unrelate to the errors found in #1.2.
 
-do not include any another preamble and postamble/summaries of your answer. If there are no errors, write 'No errors found.'.
+  **Why**: a <= 50 words paragraph explaining what caused the error.
+
+  **Suggested fix**: <= 50 words paragraph suggesting the fix for specific the error, if any. Do not discuss anything unrelate to the errors found in #why.
+
+Do not include any another preamble and postamble/summaries of your answer. If there are no errors, write 'No errors found.'.
 "
 
+
 openai_prompt=$(echo $analyze_prompt | sed 's/"/\\"/g' | sed -E 's/\\+$//g' | sed -E 's/\\+ //g')
-openai_model="gpt-4-turbo-2024-04-09"
+openai_model="gpt-4o-2024-05-13"
 openai_result=$(echo '{
     "model": "'$openai_model'",
     "temperature": 0.1,
@@ -37,8 +45,17 @@ openai_result=$(echo '{
                 -H "Authorization: Bearer $OPENAI_API_KEY" \
                 -d @-
 )
-
-throw error openai_result when is not 200
+echo '{
+    "model": "'$openai_model'",
+    "temperature": 0.1,
+    "messages": [
+    {
+        "role": "system",
+        "content": "'$openai_prompt' \n\n```'$log_output'```"
+    }
+    ]
+}'
+# throw error openai_result when is not 200
 if [ $(echo "$openai_result" | tail -n 1) != '200' ]; then
     echo "::error::OpenAI API call failed with status $openai_result: $(cat prompt_response.json)"
     exit 1
