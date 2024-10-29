6576a43057107756be56f3e0dce4f175cd326204
hendoxc <hagen.henderson@smartcontract.com>
2024-10-29 16:47:54 -0600

    Removes diagnostics

--- actions/ci-beholder-schema-validate/src/cmd/validate.go
+++ actions/ci-beholder-schema-validate/src/cmd/validate.go
@@ -21,29 +21,6 @@ func init() {
 
 func runValidateCmd(cmd *cobra.Command, args []string) {
 
-	// show cuurent working directory
-	cwd, err := os.Getwd()
-	if err != nil {
-		log.Panic(err)
-	}
-
-	fmt.Printf("Current working directory: %s\n", cwd)
-
-	// show all files and directories in the current working directory
-	files, err := os.ReadDir(cwd)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	
-	fmt.Printf("Files and directories %s\n", files)
-	
-
-	for _, file := range files {
-		fmt.Printf("File: %s\n", file.Name())
-	}
-
-
 	beholderFileContents, err := os.ReadFile(beholderFilePath)
 	if err != nil {
 		log.Panic(err)
