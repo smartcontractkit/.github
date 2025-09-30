ff8818e1de4d7a15efc81ea65027320a5a38463e
Erik Burton <erik.burton@smartcontract.com>
2025-09-30 10:40:28 -0700

    fix: go-mod-validator test.sh

--- apps/go-mod-validator/scripts/test.sh
+++ apps/go-mod-validator/scripts/test.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# @actions/glob requires disallows any relative pathing
+abspath() { cd "$1" && pwd; }
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
 pnpm nx build go-mod-validator
 
 export GITHUB_TOKEN=$(gh auth token)
@@ -13,9 +18,8 @@ tmp_file=$(mktemp)
 export GITHUB_STEP_SUMMARY="$tmp_file"
 
 export INPUT_GITHUB_TOKEN="$GITHUB_TOKEN"
-export INPUT_GO_MOD_DIR="/Users/erik/Documents/repos/chainlink-common"
+export INPUT_GO_MOD_DIR="${INPUT_GO_MOD_DIR:-$(abspath "${REPO_ROOT}/../chainlink-common")}"
 export INPUT_DEP_PREFIX="github.com/smartcontractkit"
-
 export CL_LOCAL_DEBUG="true"
 
 node apps/go-mod-validator/dist/index.js
