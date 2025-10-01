b40e6a26e8e41054c5153412387ee07735a8f72e
Erik Burton <erik.burton@smartcontract.com>
2025-10-01 14:34:23 -0700

    feat: refactor output formatting, better diffs

--- actions/apidiff-go/src/run.ts
+++ actions/apidiff-go/src/run.ts
@@ -1,10 +1,13 @@
 import * as core from "@actions/core";
 
-import { installApidiff, runApidiff } from "./apidiff";
+import { parseApidiffOutputs, installApidiff, runApidiff } from "./apidiff";
 import { setupWorktree, cleanupWorktrees } from "./git-worktree";
 import { getSummaryUrl, upsertPRComment } from "./github";
 import { CL_LOCAL_DEBUG, getInputs, getInvokeContext } from "./run-inputs";
-import { parseApidiffOutputs, formatApidiffMarkdown } from "./string-processor";
+import {
+  formatApidiffMarkdown,
+  formatApidiffJobSummary,
+} from "./string-processor";
 
 export async function run(): Promise<void> {
   try {
@@ -37,9 +40,7 @@ export async function run(): Promise<void> {
 
     // 4. Format and output results
     core.startGroup("Formatting and Outputting Results");
-    const markdownOutputAll = formatApidiffMarkdown(parsedOutputs, "", true);
-    await core.summary.addRaw(markdownOutputAll).write();
-
+    formatApidiffJobSummary(parsedOutputs);
     if (context.prNumber) {
       const summaryUrl = await getSummaryUrl(
         context.token,
