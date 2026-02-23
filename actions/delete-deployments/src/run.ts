74bb05a438d087423beb4ec96ffef9a809407386
Erik Burton <erik.burton@smartcontract.com>
2026-02-23 12:53:48 -0800

    feat: delete-deployments reusable action

---
+++ actions/delete-deployments/src/run.ts
@@ -0,0 +1,100 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+
+export type Octokit = ReturnType<typeof github.getOctokit>;
+
+import {
+  getOctokit,
+  listDeployments,
+  createDeploymentStatus,
+  deleteDeployment,
+} from "./github";
+import { getInputs, getInvokeContext } from "./run-inputs";
+
+export async function run(): Promise<void> {
+  try {
+    // 1. Get inputs and context
+    core.startGroup("Inputs and Context");
+    const context = getInvokeContext();
+    core.info(
+      `Extracted Context: ${JSON.stringify({ context, ...{ token: "<redacted>" } }, null, 2)}`,
+    );
+
+    const inputs = getInputs();
+    if (inputs.numOfPages === "all" && inputs.startingPage != null) {
+      throw new Error(`Cannot use STARTING_PAGE with NUM_OF_PAGES=all`);
+    }
+    core.info(`Extracted Inputs: ${JSON.stringify(inputs, null, 2)}`);
+
+    const octokit = getOctokit(context.token);
+    core.endGroup();
+
+    // 2. Get deployments
+    core.startGroup("Getting deployments");
+
+    const [owner, repo, ..._] = inputs.repository.split("/");
+    const deployments = await listDeployments(
+      octokit,
+      owner,
+      repo,
+      inputs.environment,
+      inputs.ref,
+      {
+        numOfPages: inputs.numOfPages,
+        startingPage: inputs.startingPage,
+      },
+    );
+
+    const deploymentIds = deployments.map((d) => d.id);
+    core.info(`Found ${deploymentIds.length} deployments to delete.`);
+    core.endGroup();
+
+    // 3. Delete deployments
+    core.startGroup("Deleting deployments");
+    await deleteDeployments(octokit, owner, repo, deploymentIds, inputs.dryRun);
+    core.endGroup();
+  } catch (error) {
+    core.endGroup();
+    core.setFailed(`Action failed: ${error}`);
+  }
+}
+
+async function deleteDeployments(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  deploymentIds: number[],
+  dryRun: boolean,
+) {
+  core.info(`Deleting deployments (${deploymentIds.length})`);
+
+  const deleteDeployments = deploymentIds.map(async (id) => {
+    if (dryRun) {
+      core.info(`[Dry Run] Would delete deployment with id ${id}`);
+      return;
+    }
+
+    const status = await createDeploymentStatus(octokit, owner, repo, id);
+    if (status) {
+      core.info(
+        `Set deployment status to 'inactive' for deployment with id ${id}`,
+      );
+    } else {
+      return false;
+    }
+
+    const deletion = await deleteDeployment(octokit, owner, repo, id);
+    if (deletion) {
+      core.info(`Deleted deployment with id ${id}`);
+      return true;
+    }
+
+    return false;
+  });
+
+  const processed = await Promise.all(deleteDeployments);
+  const succeeded = processed.filter((p) => !!p);
+  core.info(
+    `Successfully deleted ${succeeded.length}/${processed.length} deployments`,
+  );
+}
