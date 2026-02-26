7574867e59a5e675dcf4f2395105de4d21bea7b1
Erik Burton <erik.burton@smartcontract.com>
2026-02-26 15:58:40 -0800

    chore: update dist, readme

--- actions/apidiff-go/scripts/test.sh
+++ actions/apidiff-go/scripts/test.sh
@@ -4,16 +4,16 @@ pnpm nx build apidiff-go
 
 export GITHUB_TOKEN=$(gh auth token)
 export GITHUB_REPOSITORY="smartcontractkit/chainlink-common"
-export GITHUB_EVENT_NAME="pull_request"
+export GITHUB_EVENT_NAME="workflow_dispatch"
 export GITHUB_EVENT_PATH="actions/apidiff-go/scripts/payload.json"
 
 tmp_file=$(mktemp)
 export GITHUB_STEP_SUMMARY="$tmp_file"
 
 export INPUT_REPOSITORY_ROOT="/Users/erik/Documents/repos/chainlink-common"
-export INPUT_MODULE_DIRECTORY="./pkg/chipingress"
-export INPUT_BASE_REF_OVERRIDE="pkg/chipingress/v0.0.7"
-export INPUT_HEAD_REF_OVERRIDE="pkg/chipingress/v0.0.8"
+export INPUT_MODULE_DIRECTORY="."
+export INPUT_BASE_REF_OVERRIDE="v0.10.0"
+export INPUT_HEAD_REF_OVERRIDE="main"
 export INPUT_ENFORCE_COMPATIBLE="true"
 export INPUT_POST_COMMENT="false"
 export INPUT_APIDIFF_VERSION="latest"
